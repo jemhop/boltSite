a6ea210e12136313cdd2b069798ebee0ac388b41
Jemhop <emma.jellemabutler@gmail.com>
2021-09-23 14:42:16 +0200

    newwww visitor counter.... bigger logo :>>>>>>>>>

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
+var count = 1;
+
+var visits = parseInt(localStorage.getItem("boltVisits"));
+console.log(visits);
+
+if (visits === null) {
+    localStorage.setItem("boltVisits", 1);
+}
+else {
+    localStorage.setItem("boltVisits", (visits += 1));
+    console.log(localStorage.getItem("boltVisits"));
+    count = localStorage.getItem("boltVisits");
+}
+
+document.getElementById("visits").innerHTML = count.toString();
+
 document.getElementById("music").volume = 0.3;
 document.getElementById("music").playbackRate = 2.5;
 
-console.log("JS loaded")
+console.log("JS loaded");
